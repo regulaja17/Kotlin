8784f036c48a768141ab8c2b233955a2905b70a1
regulaja17 <45813830+regulaja17@users.noreply.github.com>
2019-01-09 20:54:16 +0100

    third commit

--- app/src/main/java/at/fh/swengb/regula/homeexercise2/AddNoteActivity.kt
+++ app/src/main/java/at/fh/swengb/regula/homeexercise2/AddNoteActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_add_note.*
 
 class AddNoteActivity : AppCompatActivity() {
@@ -23,19 +24,28 @@ class AddNoteActivity : AppCompatActivity() {
         val note = Note(title, content)
         db = NotesRoomDatabase.getDatabase(this)
 
+        val roomList = db.noteDao.findAll()
+
         if (db.noteDao.findNote(title) != null) {
             db.noteDao.update(note)
+            noteAdapter.updateList(roomList)
+
+            finish()
+        }
+        else if (title == "" && content == "" ) {
+            val textStart = "Please insert a valid note!"
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, textStart, duration)
+            toast.show()
         }
         else {
             db.noteDao.insert(note)
+            noteAdapter.updateList(roomList)
+
+            finish()
         }
 
-        val roomList = db.noteDao.findAll()
-        noteAdapter.updateList(roomList)
 
-        val intent = Intent(this, NoteListActivity::class.java)
-        startActivity(intent)
 
-        finish()
     }
 }
